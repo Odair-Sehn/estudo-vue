fafb3d37311a2a188158056afb73471247f1ecec
odair.sehn <90216990+Oda-Sehn@users.noreply.github.com>
2022-03-11 17:18:15 -0300

    desenvolvimento

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -4,21 +4,31 @@ new Vue({
         vidaJogador: 100,
         vidaMonstro: 100,
         jogoAtivo: false,
-        width: "100"
     },
     computed: {
     },
     methods: {
         verVida() {
-            if (this.vidaJogador == 0) {
+            if (this.vidaJogador <= 0) {
+                this.vidaJogador = 0
                 return ("Você perdeu :(")
-            } else if (this.vidaMonstro == 0) {
+            } else if (this.vidaMonstro <= 0) {
+                this.vidaMonstro = 0
                 return ("Você ganhou :)")
+            } else {
+                return ("Boa sorte, confiamos em você!")
             }
         },
+        desistir() {
+            this.vidaJogador = 100
+            this.vidaMonstro = 100
+            this.jogoAtivo = false
+        },
         ataque() {
-            let dano = 10
-            this.width += `${dano}%`
+            let atkJogador = Math.floor(Math.random(0,1) * 20)
+            let atkMonstro = Math.floor(Math.random(0,1) * 13)
+            this.vidaMonstro -= atkJogador
+            this.vidaJogador -= atkMonstro
         }
     },
     watch: {
