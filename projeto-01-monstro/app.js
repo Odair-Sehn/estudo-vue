c51909c7ad6b1735467febae422bb8c9a14b1c9a
odair.sehn <90216990+Oda-Sehn@users.noreply.github.com>
2022-04-29 08:50:00 -0300

    projeto finalizado

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -4,6 +4,7 @@ new Vue({
         vidaJogador: 100,
         vidaMonstro: 100,
         jogoAtivo: false,
+        logs: []
     },
     computed: {
     },
@@ -11,24 +12,56 @@ new Vue({
         verVida() {
             if (this.vidaJogador <= 0) {
                 this.vidaJogador = 0
+                this.jogoAtivo = false
                 return ("Você perdeu :(")
             } else if (this.vidaMonstro <= 0) {
                 this.vidaMonstro = 0
+                this.jogoAtivo = false
                 return ("Você ganhou :)")
             } else {
                 return ("Boa sorte, confiamos em você!")
             }
         },
+        numeroRandom(min, max) {
+            const valor = Math.random() * (max - min) + min
+            return Math.round(valor)
+        },
+        novoJogo() {
+            this.vidaJogador = 100
+            this.vidaMonstro = 100
+            this.jogoAtivo = true
+            this.logs = []
+        },
         desistir() {
             this.vidaJogador = 100
             this.vidaMonstro = 100
             this.jogoAtivo = false
+            this.logs = []
         },
-        ataque() {
-            let atkJogador = Math.floor(Math.random(0,1) * 20)
-            let atkMonstro = Math.floor(Math.random(0,1) * 13)
-            this.vidaMonstro -= atkJogador
-            this.vidaJogador -= atkMonstro
+        ataque(especial) {
+            this.dano("vidaMonstro", 5, 10, especial, "Jogador", "Monstro", "logJogador")
+            if (this.vidaMonstro > 0) {
+                this.dano("vidaJogador", 7, 12, false, "Monstro", "Jogador", "logMonstro")
+            }
+        },
+        dano(prop, min, max, especial, origem, alvo, classe) {
+            const plus = especial ? 5 : 0
+            const dano = this.numeroRandom(min + plus, max + plus)
+            this[prop] = Math.max(this[prop] - dano, 0)
+            this.registraLog(`${origem} atingiu o ${alvo} com ${dano} de dano.`, classe)
+        },
+        
+        curar(min, max) {
+            const curar = this.numeroRandom(min, max)
+            this.vidaJogador = Math.min(this.vidaJogador + curar, 100)
+            this.registraLog(`Jogador se curou em ${curar} de vida`, "logCurar")
+        },
+        curarAtacar() {
+            this.curar(10, 14)
+            this.dano("vidaJogador", 7, 12, false, "Monstro", "Jogador", "logMonstro")
+        },
+        registraLog(texto, classe) {
+            this.logs.unshift({ texto, classe })
         }
     },
     watch: {
